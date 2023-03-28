dd33b271215395a397710a60df4a9f319e5f03f5
sdangelillo <serafino.dangelillo@lastminute.com>
2023-03-28 14:46:10 +0200

    changed test

--- rust/src/add_two_numbers/mod.rs
+++ rust/src/add_two_numbers/mod.rs
@@ -122,12 +122,12 @@ mod tests {
   }
 
   #[test]
-  fn test1() {
+  fn test4() {
   
     let l1 = Some(Box::new(ListNode{val: 2, next: Some(Box::new(ListNode { val: 4, next: Some(Box::new(ListNode::new(9))) }))}));
     let l2 = Some(Box::new(ListNode{val: 5, next: Some(Box::new(ListNode { val: 6, next: Some(Box::new(ListNode { val: 4, next: Some(Box::new(ListNode::new(9))) })) }))}));
     let solution = Solution::add_two_numbers(l1, l2);
 
-    assert_eq!(solution, Some(Box::new(ListNode{val: 7, next: Some(Box::new(ListNode { val: 0, next: Some(Box::new(ListNode::new(8))) }))})));
+    assert_eq!(solution, Some(Box::new(ListNode{val: 7, next: Some(Box::new(ListNode { val: 0, next: Some(Box::new(ListNode { val: 4, next: Some(Box::new(ListNode{val: 0, next: Some(Box::new(ListNode::new(1)))})) })) }))})));
   }
 }
