10fcfec7fefeadeb170154037f6cf0cebca9a5fb
sdangelillo <serafino.dangelillo@lastminute.com>
2023-03-29 14:51:00 +0200

    added solution for two numbers

--- rust/src/add_two_numbers/mod.rs
+++ rust/src/add_two_numbers/mod.rs
@@ -1,57 +1,70 @@
 struct Solution;
 
+// [9,9,1] + [1] = [0,0,2]
+
 impl Solution {
   pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    let mut a = l1.as_ref();
-    let mut b = l2.as_ref();
+    
+    let mut a = &l1;
+    let mut b = &l2;
 
     let mut result = ListNode::new(0);
-    let mut current = &mut result;
-  
-    while a.is_some() && b.is_some() {
+    let mut current_node = &mut result;
 
-      let sum = sum(&a?.val, &b?.val);
+    while a.is_some() || b.is_some() {
 
-      a = a?.next.as_ref();
-      b = b?.next.as_ref();
+      match (a, b) {
+        (Some(a_node), Some(b_node)) => {
+          
+          let sum_result = sum(&a_node.val, &b_node.val);
+          
+          if current_node.val + sum_result.digit < 10 {
+            current_node.val += sum_result.digit;
+          
+            if sum_result.reminder > 0 || a_node.next.is_some() || b_node.next.is_some() {
+              current_node.next = Some(Box::new(ListNode::new(sum_result.reminder)));
+              current_node = current_node.next.as_mut()?;
+            }
+          } else {
+            current_node.val = current_node.val + sum_result.digit - 10;
+          
+            current_node.next = Some(Box::new(ListNode::new(1)));
+            current_node = current_node.next.as_mut()?;
+          }
+          
+          
+          a = &a_node.next;
+          b = &b_node.next;
+        },
+        (Some(a_node), None) => {
 
-      current.val += sum.digit;
+          let sum_result = sum(&a_node.val, &current_node.val);
+          current_node.val = sum_result.digit;
 
+          if sum_result.reminder > 0 || a_node.next.is_some() {
+            current_node.next = Some(Box::new(ListNode::new(sum_result.reminder)));
+            current_node = current_node.next.as_mut()?;
+          }
 
-      if a.is_some() {
-        current.next = Some(Box::new(ListNode::new(sum.reminder)));
-        current = current.next.as_mut()?;
+          a = &a_node.next;
+        },
+        (None, Some(b_node)) => {
+
+          let sum_result = sum(&b_node.val, &current_node.val);
+          current_node.val = sum_result.digit;
+
+          if sum_result.reminder > 0 || b_node.next.is_some() {
+            current_node.next = Some(Box::new(ListNode::new(sum_result.reminder)));
+            current_node = current_node.next.as_mut()?;
+          }
+
+          b = &b_node.next;
+        },
+        _ => {},
       }
     }
 
-    match (a, b) {
-      (Some(_), None) => {
-        while a.is_some() {
-          let sum = sum(&a?.val, &current.val);
-
-          a = a?.next.as_ref();
-          current.val = sum.digit;
-
-
-          current.next = Some(Box::new(ListNode::new(sum.reminder)));
-          current = current.next.as_mut()?;
-        }
-      }
-      (None, Some(_)) => {
-        while b.is_some() {
-          let sum = sum(&current.val, &b?.val);
-
-          b = b?.next.as_ref();
-          current.val = sum.digit;
-
-          current.next = Some(Box::new(ListNode::new(sum.reminder)));
-          current = current.next.as_mut()?;
-        }
-      }
-      _ => {}
-    }
-
-    return Some(Box::new(result));
+    Some(Box::new(result))
   }
 }
 
@@ -129,5 +142,35 @@ mod tests {
     let solution = Solution::add_two_numbers(l1, l2);
 
     assert_eq!(solution, Some(Box::new(ListNode{val: 7, next: Some(Box::new(ListNode { val: 0, next: Some(Box::new(ListNode { val: 4, next: Some(Box::new(ListNode{val: 0, next: Some(Box::new(ListNode::new(1)))})) })) }))})));
+  }
+
+  #[test]
+  fn test5() {
+  
+    let l1 = Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode { val: 9, next: Some(Box::new(ListNode::new(1))) }))}));
+    let l2 = Some(Box::new(ListNode::new(1)));
+    let solution = Solution::add_two_numbers(l1, l2);
+
+    assert_eq!(solution, Some(Box::new(ListNode{val: 0, next: Some(Box::new(ListNode { val: 0, next: Some(Box::new(ListNode::new(2))) }))})));
+  }
+
+  #[test]
+  fn test6() {
+  
+    let l1 = Some(Box::new(ListNode::new(0)));
+    let l2 = Some(Box::new(ListNode { val: 7, next: Some(Box::new(ListNode::new(3))) }));
+    let solution = Solution::add_two_numbers(l1, l2);
+
+    assert_eq!(solution, Some(Box::new(ListNode { val: 7, next: Some(Box::new(ListNode::new(3))) })));
+  }
+
+  #[test]
+  fn test7() {
+  
+    let l1 = Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode::new(7))) }));
+    let l2 = Some(Box::new(ListNode { val: 9, next: Some(Box::new(ListNode::new(2))) }));
+    let solution = Solution::add_two_numbers(l1, l2);
+
+    assert_eq!(solution, Some(Box::new(ListNode{val: 2, next: Some(Box::new(ListNode { val: 0, next: Some(Box::new(ListNode::new(1))) }))})));
   }
 }
